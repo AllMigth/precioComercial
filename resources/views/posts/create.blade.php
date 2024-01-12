1e13b4814500ecde53c305b1f621f6fe667063a2
AllMigth <jgarzon292@gmail.com>
2024-01-11 19:35:47 -0500

    validacion de formulario, para que no se vayan campos vacios y se mantenga la infor despues de validar los campos

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,12 +9,22 @@
     @csrf
     <label >
         Title <br>
-        <input name="title" type="text">
-    </label>    <br>
+        <input name="title" type="text" value="{{old('title')}}">
+    </label>   
+    @error('title')
+    <br>
+    <small style="color: red"> {{ $message }} </small>
+    @enderror
+    <br>
     <label>
-        Body<br>
-        <textarea name="body"></textarea>
-    </label> <br>
+        Body <br>
+        <textarea name="body">{{old('body')}}</textarea>
+    </label> 
+    @error('body')
+    <br>
+    <small style="color: red"> {{ $message }} </small>
+    @enderror
+    <br>
     <button type="submit">Send</button>
 </form>
 
